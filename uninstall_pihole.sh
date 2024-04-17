2941501b23b1b0b28a9ebd4662895f95b5208dfb
Michael Bierman <mbierman@users.noreply.github.com>
2024-04-17 06:28:58 -0700

    Update uninstall_pihole.sh

--- uninstall_pihole.sh
+++ uninstall_pihole.sh
@@ -5,6 +5,7 @@ sudo docker container stop pihole && sudo docker container rm pihole
 echo -e "\n\nYou may also want to run \`sudo rm -rf /data/pi-hole\` but that will remove all pi-hole settings so be cautious. \n\n"
 rm /home/pi/.firewalla/run/docker/pi-hole/docker-compose.yaml
 sudo systemctl stop docker-compose@pi-hole
+sudo rm -rf /home/pi/.firewalla/run/docker/pi-hole/etc-dnsmasq.d
 rm /home/pi/.firewalla/config/post_main.d/start_pihole.sh
 sudo docker stop cloudflared
 sudo docker stop cloudflared && sudo docker container rm cloudflared
